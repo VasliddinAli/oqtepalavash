41e270e2ce5f9d06fdfb053dbb9639323ca08c7d
VasliddinAli <97330019+VasliddinAli@users.noreply.github.com>
2023-02-14 08:24:28 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,29 +12,14 @@ import { useLoadingBar , useMessage , useNotification } from 'naive-ui'
 import { useRoute, useRouter } from 'vue-router'
 const loading = useLoadingBar()
 const message = useMessage()
-const notification = useNotification()
 const route = useRoute()
 const router = useRouter()
 
 router.beforeEach(()=>{
   loading.start()
-  message.success(route.name)
-  notification.info({
-    content: 'What to say?',
-    meta: "I don't know",
-    duration: 2500,
-    keepAliveOnHover: true
-  })
 })
 router.afterEach(()=>{
   loading.finish()
-  message.success(route.name)
-  notification.warning({
-    content: 'What to say?',
-    meta: "I don't know",
-    duration: 2500,
-    keepAliveOnHover: true
-  })
 })
 router.onError(()=>{
   loading.error()
