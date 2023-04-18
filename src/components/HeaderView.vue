b2281ac14903ed3157213db5fbe360098fd52f7d
VasliddinAli <vasliddinali@gmail.com>
2023-04-18 16:17:07 +0500

    comment

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -54,11 +54,14 @@
       </div>
       <div class="navMob-menu">
         <n-drawer placement="left" v-model:show="show" :width="300">
-          <n-drawer-content header-style="background: #f6f8f9;font-size: 20px; height: 60px"  body-content-style="display: flex; flex-direction: column;gap: 10px;" title="Menyu" closable>
-            <n-dropdown body-content-style=" width: 100%;padding: 10px;border-radius: 10px;" trigger="hover" :options="options">
+          <n-drawer-content header-style="background: #f6f8f9;font-size: 20px; height: 60px"
+            body-content-style="display: flex; flex-direction: column;gap: 10px;" title="Menyu" closable>
+            <n-dropdown body-content-style=" width: 100%;padding: 10px;border-radius: 10px;" trigger="hover"
+              :options="options">
               <n-button>O'zbekcha</n-button>
             </n-dropdown>
-            <button style="border: 0;padding: 10px;font-size: 16px;color: #e62b40;width: 100%;text-align: left;background: #00000004;cursor: pointer;border-radius: 10px;">Kirish</button>
+            <button
+              style="border: 0;padding: 10px;font-size: 16px;color: #e62b40;width: 100%;text-align: left;background: #00000004;cursor: pointer;border-radius: 10px;">Kirish</button>
             <p style="width: 100%;height: 1px;background: #0000001e; margin: 10px 0;"></p>
             <div class="Moblinks">
               <router-link @click="show = false" class-active="active" to="/">Menyu</router-link>
@@ -68,6 +71,34 @@
             </div>
           </n-drawer-content>
         </n-drawer>
+      </div>
+
+
+      <div class="nav_bottom">
+        <router-link class-active="active" to="/">
+          <div class="bolim">
+            <div class="icon"><i class="fa-solid fa-house"></i></div>
+            <div class="label">Bosh sahifa</div>
+          </div>
+        </router-link>
+        <router-link class-active="active" to="/branches">
+          <div class="bolim">
+            <div class="icon"><i class="fa-solid fa-code-branch"></i></div>
+            <div class="label">Filiallar</div>
+          </div>
+        </router-link>
+        <router-link class-active="active" to="/contact">
+          <div class="bolim">
+            <div class="icon"><i class="fa-solid fa-phone-volume"></i></div>
+            <div class="label">Bog'lanish</div>
+          </div>
+        </router-link>
+        <router-link class-active="active" to="/cart">
+          <div class="bolim">
+            <div class="icon"><i class="fa-solid fa-cart-shopping"></i></div>
+            <div class="label">Savat</div>
+          </div>
+        </router-link>
       </div>
     </div>
   </section>
@@ -104,11 +135,11 @@ export default defineComponent({
     };
   },
   methods: {
-    dropdown(){
-      if(this.dropList != true){
+    dropdown() {
+      if (this.dropList != true) {
         this.dropList = true
       }
-      else{
+      else {
         this.dropList = false
       }
     }
